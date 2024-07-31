be783dfe075a691fdd4169bb2ed78c8a28cea687
MkBaek0229 <alsrl6678@naver.com>
2024-07-31 14:01:08 +0900

    first commit

---
+++ Regi_/Controllers/StaffController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace T1.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        public IActionResult Authentication()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ Regi_/Controllers/StudentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+
+namespace Regi.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        public IActionResult Authentication()
+        {
+            return View();
+        }
+
+        public IActionResult AccountRegistration()
+        {
+            ViewBag.SchoolName = "소속 기관 이름";
+            ViewBag.DisplayName = "사용자 이름";
+            ViewBag.Admission = "2019";
+            return View();
+        }
+
+        public IActionResult Success()
+        {
+            ViewBag.DisplayName = "사용자 이름";
+            ViewBag.Admission = "입학년도";
+            ViewBag.UserPrincipalName = "사용자 계정";
+            return View();
+        }
+    }
+}
